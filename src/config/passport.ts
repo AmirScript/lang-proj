68508d86339546e90a911b5e13fd1dbf79407c37
AmirScript <amir.space76@gmail.com>
2024-11-13 19:55:07 +0330

    x

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -2,10 +2,16 @@ import prisma from '../prismaClient';
 import passport from 'passport';
 import {ExtractJwt, Strategy} from 'passport-jwt';
 
+interface JwtPayload {
+  id: number;
+}
+
+type DoneCallback = (error: any, user?: any) => void;
+
 passport.use(new Strategy({
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET as string
-}, async (jwt_payload, done) => {
+}, async (jwt_payload: JwtPayload, done: DoneCallback) => {
   try {
     const user = await prisma.user.findUnique({ where: { id: jwt_payload.id } });
     if (user) {
